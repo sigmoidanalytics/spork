e165ea7ae7a83aab1ec83e3e6914e2b8c0077bae
dvryaboy <dvryaboy@gmail.com>
2012-07-11 17:51:01 -0700

    add POCache

--- src/org/apache/pig/backend/hadoop/executionengine/physicalLayer/plans/PhyPlanVisitor.java
+++ src/org/apache/pig/backend/hadoop/executionengine/physicalLayer/plans/PhyPlanVisitor.java
@@ -28,10 +28,10 @@ import org.apache.pig.impl.plan.VisitorException;
 
 /**
  * The visitor class for the Physical Plan. To use this,
- * create the visitor with the plan to be visited. Call 
+ * create the visitor with the plan to be visited. Call
  * the visit() method to traverse the plan in a depth first
  * fashion.
- * 
+ *
  * This class also visits the nested plans inside the operators.
  * One has to extend this class to modify the nature of each visit
  * and to maintain any relevant state information between the visits
@@ -47,15 +47,15 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
     public void visitLoad(POLoad ld) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitStore(POStore st) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitNative(PONative nat) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitFilter(POFilter fl) throws VisitorException{
         pushWalker(mCurrentWalker.spawnChildWalker(fl.getPlan()));
         visit();
@@ -71,7 +71,7 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
             popWalker();
         }
     }
-    
+
     public void visitLocalRearrange(POLocalRearrange lr) throws VisitorException{
         List<PhysicalPlan> inpPlans = lr.getPlans();
         for (PhysicalPlan plan : inpPlans) {
@@ -84,19 +84,19 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
     public void visitGlobalRearrange(POGlobalRearrange gr) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitPackage(POPackage pkg) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitCombinerPackage(POCombinerPackage pkg) throws VisitorException{
         //do nothing
     }
- 
+
     public void visitMultiQueryPackage(POMultiQueryPackage pkg) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitPOForEach(POForEach nfe) throws VisitorException {
         List<PhysicalPlan> inpPlans = nfe.getInputPlans();
         for (PhysicalPlan plan : inpPlans) {
@@ -105,11 +105,11 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
             popWalker();
         }
     }
-    
+
     public void visitUnion(POUnion un) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitSplit(POSplit spl) throws VisitorException{
         List<PhysicalPlan> plans = spl.getPlans();
         for (PhysicalPlan plan : plans) {
@@ -127,9 +127,9 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
             popWalker();
         }
     }
-    
+
 	public void visitDistinct(PODistinct distinct) throws VisitorException {
-        //do nothing		
+        //do nothing
 	}
 
 	public void visitSort(POSort sort) throws VisitorException {
@@ -140,66 +140,66 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
             popWalker();
         }
 	}
-    
+
     public void visitConstant(ConstantExpression cnst) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitProject(POProject proj) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitGreaterThan(GreaterThanExpr grt) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitLessThan(LessThanExpr lt) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitGTOrEqual(GTOrEqualToExpr gte) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitLTOrEqual(LTOrEqualToExpr lte) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitEqualTo(EqualToExpr eq) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitNotEqualTo(NotEqualToExpr eq) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitRegexp(PORegexp re) throws VisitorException{
         //do nothing
     }
 
     public void visitIsNull(POIsNull isNull) throws VisitorException {
     }
-    
+
     public void visitAdd(Add add) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitSubtract(Subtract sub) throws VisitorException {
         //do nothing
     }
-    
+
     public void visitMultiply(Multiply mul) throws VisitorException {
         //do nothing
     }
-    
+
     public void visitDivide(Divide dv) throws VisitorException {
         //do nothing
     }
-    
+
     public void visitMod(Mod mod) throws VisitorException {
         //do nothing
     }
-    
+
     public void visitAnd(POAnd and) throws VisitorException {
         //do nothing
     }
@@ -214,62 +214,62 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
 
     public void visitBinCond(POBinCond binCond) {
         // do nothing
-        
+
     }
 
     public void visitNegative(PONegative negative) {
         //do nothing
-        
+
     }
-    
+
     public void visitUserFunc(POUserFunc userFunc) throws VisitorException {
         //do nothing
     }
-    
+
     public void visitComparisonFunc(POUserComparisonFunc compFunc) throws VisitorException {
         //do nothing
     }
 
     public void visitMapLookUp(POMapLookUp mapLookUp) {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
     public void visitJoinPackage(POJoinPackage joinPackage) throws VisitorException{
         //do nothing
     }
 
     public void visitCast(POCast cast) {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
     public void visitLimit(POLimit lim) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitCross(POCross cross) throws VisitorException{
         //do nothing
     }
-    
+
     public void visitFRJoin(POFRJoin join) throws VisitorException {
         //do nothing
     }
-    
+
     public void visitMergeJoin(POMergeJoin join) throws VisitorException {
         //do nothing
     }
-    
+
     public void visitMergeCoGroup(POMergeCogroup mergeCoGrp) throws VisitorException{
-        
+
     }
     /**
      * @param stream
-     * @throws VisitorException 
+     * @throws VisitorException
      */
     public void visitStream(POStream stream) throws VisitorException {
         // TODO Auto-generated method stub
-        
+
     }
 
 	public void visitSkewedJoin(POSkewedJoin sk) throws VisitorException {
@@ -290,7 +290,7 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
      */
     public void visitPOOptimizedForEach(POOptimizedForEach optimizedForEach) throws VisitorException {
         // TODO Auto-generated method stub
-        
+
     }
 
     /**
@@ -302,6 +302,10 @@ public class PhyPlanVisitor extends PlanVisitor<PhysicalOperator,PhysicalPlan> {
     }
 
     public void visitPartialAgg(POPartialAgg poPartialAgg) {
+    }
+
+    public void visitCache(POCache poCache) {
+
     }
 
 
